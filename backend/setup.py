ba15e54ad36f7a19d01ae268739ee0d9bd88280e
Julian Pollinger <julian@pollinger.dev>
2023-12-08 12:14:52 +0100

    fix: fix cost measuring by updating dependencies

--- backend/setup.py
+++ backend/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-requirements = ['jsonschema==4.19.2', 'argparse==1.4.0', 'pyyaml==6.0.1', 'langchain==0.0.335', 'typesense==0.17.0',
-                'elasticsearch==8.11.0', 'websockets==12.0', 'peewee==3.17.0', 'deepl==1.16.1', 'openai==1.3.5',
+requirements = ['jsonschema==4.19.2', 'argparse==1.4.0', 'pyyaml==6.0.1', 'langchain==0.0.348', 'typesense==0.17.0',
+                'elasticsearch==8.11.0', 'websockets==12.0', 'peewee==3.17.0', 'deepl==1.16.1', 'openai==1.3.7',
                 'pip==23.2.1', 'attrs==23.1.0', 'idna==3.4', 'multidict==6.0.4', 'anyio==3.7.1', 'sniffio==1.3.0',
                 'requests==2.31.0', 'numpy==1.26.2', 'setuptools==65.5.0', 'yarl==1.9.2', 'aiohttp==3.8.6',
                 'aiosignal==1.3.1', 'frozenlist==1.4.0', 'certifi==2023.7.22', 'greenlet==3.0.1', 'pydantic==2.5.0',
